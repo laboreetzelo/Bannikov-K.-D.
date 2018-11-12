1da0a2fb638b65d3711c798b1c5bfc76c1988be4
laboreetzelo <44449586+laboreetzelo@users.noreply.github.com>
2018-11-13 03:25:31 +0400

    Add files via upload

---
+++ BAK/BAK/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAK
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Number = new Chislo();
+            var Prover = new Proverka();
+            int number;
+            int[] nums = new int[4];
+            Number.Chislo1();
+            Console.WriteLine();
+            Console.Write("Введите число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Prover.Proverka1(number, nums);
+            Console.ReadKey();
+        }
+    }
+}
